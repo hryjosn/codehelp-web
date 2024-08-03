265d8de9bed50432876cb256eb4e7bf6e53e6269
nonoLin <denkanono@gmail.com>
2024-08-03 22:15:44 +0800

    feat: props cureent mentor data to Bio component

--- src/components/mentor/Bio/index.tsx
+++ src/components/mentor/Bio/index.tsx
@@ -1,26 +1,33 @@
 import Image from 'next/image'
 
-const Bio = () => {
+type Props = {
+    avatar: string
+    name: string
+    company: string
+    title: string
+}
+
+const Bio = ({ avatar, name, company, title }: Props) => {
     return (
         <div className="flex flex-col gap-4 md:flex-row md:items-center md:gap-6">
-            <div className="overflow-hidden w-32 rounded-full md:w-48">
+            <div className="overflow-hidden w-32 h-32 rounded-full md:w-48 md:h-48">
                 <Image
-                    src={'/MentorList/mentor_1.jpg'}
-                    alt={'name'}
+                    src={avatar}
+                    alt={name}
                     width={0}
                     height={0}
                     sizes="100%"
-                    style={{ width: '100%', height: 'auto' }}
+                    style={{ width: 'auto', height: 'auto' }}
                 />
             </div>
             <div>
                 <h2 className="text-2xl text-slate-800 font-bold mb-1 md:text-4xl">
-                    Vamsi Yarlagadda
+                    {name}
                 </h2>
                 <p className="text-base	md:text-lg">
-                    HCM/Payroll Solutions Architect
+                    <span>{title}</span>
                     <span className="mx-1 text-gray-500">at</span>
-                    Seattle school district
+                    <span>{company}</span>
                 </p>
             </div>
         </div>
