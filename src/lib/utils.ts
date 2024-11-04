8c62cfe2095ce5ce568e55ac51702554848707e2
KevinLai02 <jihn.smith629@gmail.com>
2024-11-04 18:58:54 +0800

    adjust socket url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,6 @@ import { ServerToClientEvents, ClientToServerEvents } from './types'
 export const cn = (...input: ClassValue[]) => twMerge(clsx(input))
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-    'http://localhost:80',
+    process.env.NEXT_PUBLIC_API_URL,
     { transports: ['websocket'] }
 )
