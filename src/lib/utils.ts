3f4a31fe2aebd168721a1bb826e19bc9802ed424
KevinLai02 <jihn.smith629@gmail.com>
2024-11-20 18:09:43 +0800

    remove unnecessary import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
 import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { io, Socket } from 'socket.io-client'
-import { ServerToClientEvents, ClientToServerEvents } from './types'
 
 export const cn = (...input: ClassValue[]) => twMerge(clsx(input))
