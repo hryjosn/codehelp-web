e6e19185a37b380528b957bb8ee13a34a6d34b66
andyWeb2022 <andy072892live@gmail.com>
2024-11-30 21:30:02 +0800

    Fix useGetChatroomList not found issue

--- src/api/chatroom/types.ts
+++ src/api/chatroom/types.ts
@@ -1,5 +1,6 @@
-import { ChatroomInfoT } from '~/container/Chat/store/type'
 
+import { ChatroomInfoT } from '~/container/Chat/store/type'
+import { ChatroomListT } from '~/container/Chat/store/type'
 export interface CreateChatroomReqT {}
 export interface CreateChatroomResT {
     chatroomId: string
@@ -22,4 +23,9 @@ export interface NewMessageResT {
         chatroom: ChatroomInfoT
     }
     status: string
+}
+export interface ChatroomListResT {
+    chatroomList: ChatroomListT[]
+    status: string
+    total: number
 }
