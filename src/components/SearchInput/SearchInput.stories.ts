e108d6981591d3e5fde8010a6c7720f710a9880e
KevinLai02 <jihn.smith629@gmail.com>
2025-01-18 20:26:44 +0800

    add args in SearchInput storybook

--- src/components/SearchInput/SearchInput.stories.ts
+++ src/components/SearchInput/SearchInput.stories.ts
@@ -14,4 +14,11 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+    args: {
+        value: '',
+        placeholder: 'Search members',
+        onChange(e) {},
+        onSubmit() {},
+    },
+}
