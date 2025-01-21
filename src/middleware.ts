9ff272c622d9c170205359f98888d6d1e2b103a4
hryjosn <jo74705@gmail.com>
2025-01-21 20:50:09 +0800

    Add middleware function for request handling

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    console.log('middleware')
+}
+export const config = {
+    matcher: ['/'],
+}
