ad863d525f07e37e73cf50a1d4f96765d97c5c1d
andyWeb2022 <andy072892live@gmail.com>
2025-02-05 23:56:32 +0800

    Fix build error

--- src/container/MentorProfile/MentorProfile.stories.ts
+++ src/container/MentorProfile/MentorProfile.stories.ts
@@ -15,4 +15,8 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = { args: { params: { id: '1' } } }
+export const Default: Story = {
+    args: {
+        params: Promise.resolve({ id: '1' }),
+    },
+}
