6df8a7c05b1ec1e098ab5a40253164259dfdf617
KevinLai02 <jihn.smith629@gmail.com>
2025-03-29 15:32:16 +0800

    add Card component and CardHeader component

---
+++ src/container/UserProfile/components/Card/Card.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Card from './Card'
+import { fn } from '@storybook/test'
+
+const meta = {
+    title: 'UserProfile/Card',
+    component: Card,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    args: {
+        onClick: fn(),
+    },
+} satisfies Meta<typeof Card>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        className: '',
+        headerTitle: 'About Me',
+        onClick: fn(),
+        content: <p>here is content</p>,
+    },
+}
---
+++ src/container/UserProfile/components/Card/types.ts
@@ -0,0 +1,6 @@
+export interface Props {
+    className?: string
+    headerTitle?: string | React.ReactNode
+    content?: React.ReactNode
+    onClick?: () => void
+}
