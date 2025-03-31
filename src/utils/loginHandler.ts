e03bc5d175a218bb368e678a11a28b6b1e1de119
KevinLai02 <jihn.smith629@gmail.com>
2025-03-31 12:40:16 +0800

    resolve login error message issue

--- src/utils/loginHandler.ts
+++ src/utils/loginHandler.ts
@@ -2,11 +2,22 @@ import axios from 'axios'
 import { getSession, signIn } from 'next-auth/react'
 
 const loginHandler = async (data: { email: string; password: string }) => {
-    await signIn('credentials', {
+    const res = await signIn('credentials', {
         email: data.email,
         password: data.password,
         callbackUrl: '/',
+        redirect: false,
     })
+
+    if (res?.error) {
+        let errorData
+        try {
+            errorData = JSON.parse(res.error)
+        } catch {
+            errorData = { message: res.error }
+        }
+        return errorData
+    }
 
     const session = await getSession()
     if (session?.accessToken) {
