1351ac61270868a26585d39d47f729717616e75a
hryjosn <jo74705@gmail.com>
2025-04-29 21:43:56 +0800

    add TURN server configurations and update shareScreen functionality

--- src/container/VideoConference/constant.ts
+++ src/container/VideoConference/constant.ts
@@ -10,6 +10,31 @@ export const PC_CONFIG = {
         { urls: 'stun:stun3.l.google.com:5349' },
         { urls: 'stun:stun4.l.google.com:19302' },
         { urls: 'stun:stun4.l.google.com:5349' },
+        {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+        },
+        {
+            url: 'turn:turn.bistri.com:80',
+            credential: 'homeo',
+            username: 'homeo',
+        },
+        {
+            url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            credential: 'webrtc',
+            username: 'webrtc',
+        },
     ],
 }
 
