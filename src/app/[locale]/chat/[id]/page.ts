4e6ae4faa73d29fd1f79a9f5dc08d4588f400ee5
KevinLai02 <jihn.smith629@gmail.com>
2025-07-14 15:35:52 +0800

    setting chattingArea Web title

--- src/app/[locale]/chat/[id]/page.ts
+++ src/app/[locale]/chat/[id]/page.ts
@@ -1,1 +1,27 @@
+import { Metadata } from 'next'
+import { callGetChatroomInfoHandler } from '~/api/chatroom/chatroomAPI'
+import { callGetUserInfoHandler } from '~/api/user/userAPI'
+
+type Props = {
+    params: Promise<{ id: string }>
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { id: chatroomId } = await params
+
+    const userData = await callGetUserInfoHandler()
+    const chatroomDataRes = await callGetChatroomInfoHandler(chatroomId)
+
+    const isIAmMentor =
+        userData.user.id === chatroomDataRes?.chatroom?.mentor.id
+
+    const otherUserName = isIAmMentor
+        ? chatroomDataRes!.chatroom.member.userName
+        : chatroomDataRes!.chatroom.mentor.userName
+
+    return {
+        title: otherUserName,
+    }
+}
+
 export { default } from '~/container/Chat/Chat'
