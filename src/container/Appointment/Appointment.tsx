3bf7f96b0e92fdab0a9662b75ed3caf1a161a7ad
KevinLai02 <jihn.smith629@gmail.com>
2025-12-18 10:33:56 +0800

    refactor appointment page and add AppointmentSection component

--- src/container/Appointment/Appointment.tsx
+++ src/container/Appointment/Appointment.tsx
@@ -1,120 +1,17 @@
-'use client'
+import { callGetUserInfoHandler } from '~/api/user/userAPI'
+import AppointmentSection from './components/AppointmentSection/AppointmentSection'
+import { USER_IDENTITY } from '~/api/user/types'
 
-import React, { useState } from 'react'
-import ButtonGrid from './components/ButtonGrid/ButtonGrid'
-import TimeZoneGrid from './components/TimeZoneGrid/TimeZoneGrid'
-import WeekGrid from './components/WeekGrid/WeekGrid'
-import HourGrid from './components/HourGrid/HourGrid'
-import Header from '~/components/Header/Header'
-import { AppointmentT, Days, SelectedItem } from './store/types'
-import { Button } from '~/components/Button/Button'
-import { useUpdateAvailableTime } from '~/api/mentor/mentor'
-
-const DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-const TIMECODES = [...Array(48)]
-
-const Appointment = () => {
-    const [selectedItems, setSelectedItems] = useState<SelectedItem>({})
-    const { mutate: saveAppointment } = useUpdateAvailableTime()
-
-    const setting = (day: number, time: number) => {
-        setSelectedItems((prevState) => {
-            const newItems = { ...prevState }
-            const key = `${day}-${time}`
-            if (newItems[key]) {
-                delete newItems[key]
-            } else {
-                newItems[key] = { day, time }
-            }
-            return newItems
-        })
-    }
-    const reset = () => {
-        const result = window.confirm(
-            'Are you sure to reset appointment time？'
-        )
-        if (result) {
-            setSelectedItems({})
-        }
-    }
-    const save = () => {
-        const selectedList = Object.values(selectedItems)
-        let appointment: AppointmentT[] = []
-
-        selectedList.forEach(({ day, time }) => {
-            const matchedDay = appointment.find(
-                (data) => data.day === Days[day]
-            )
-
-            if (matchedDay) {
-                matchedDay.timeCode.push(time)
-            } else {
-                appointment.push({
-                    day: Days[day],
-                    timeCode: [time],
-                })
-            }
-        })
-        saveAppointment(
-            { availableTimeList: appointment },
-            {
-                onSuccess(res) {
-                    alert(res.message)
-                },
-            }
-        )
-    }
+export default async function AppointmentPage() {
+    const userData = await callGetUserInfoHandler()
 
     return (
-        <>
-            <Header />
-            <div className="flex justify-center gap-10 py-4">
-                <table className="w-full max-w-5xl">
-                    <thead>
-                        <tr className="flex">
-                            <TimeZoneGrid />
-                            {DAYS.map((week, index) => (
-                                <WeekGrid key={index} week={week} />
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {TIMECODES.map((_, rowIndex) => (
-                            <tr key={rowIndex} className="flex">
-                                <HourGrid hour={rowIndex} />
-                                {DAYS.map((_, colIndex) => (
-                                    <ButtonGrid
-                                        key={colIndex}
-                                        variant={
-                                            selectedItems[
-                                                `${colIndex + 1}-${rowIndex}`
-                                            ]
-                                                ? 'primary'
-                                                : 'secondary'
-                                        }
-                                        onClick={() => {
-                                            setting(colIndex + 1, rowIndex)
-                                        }}
-                                    />
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <div className="flex flex-col items-center gap-5 pt-4">
-                    <p className="mb-5 text-3xl">
-                        Select your availability Times
-                    </p>
-                    <Button className="min-w-40 max-w-80" onClick={reset}>
-                        Reset All
-                    </Button>
-                    <Button className="min-w-40 max-w-80" onClick={save}>
-                        Save
-                    </Button>
-                </div>
-            </div>
-        </>
+        <AppointmentSection
+            mentorAvailableTimes={
+                userData.identity === USER_IDENTITY.MENTOR
+                    ? (userData.user?.mentorAvailableTimes ?? [])
+                    : []
+            }
+        />
     )
 }
-
-export default Appointment
---
+++ src/container/Appointment/components/AppointmentSection/AppointmentSection.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import React, { use, useEffect } from 'react'
+import ButtonGrid from '../ButtonGrid/ButtonGrid'
+import TimeZoneGrid from '../TimeZoneGrid/TimeZoneGrid'
+import WeekGrid from '../WeekGrid/WeekGrid'
+import HourGrid from '../HourGrid/HourGrid'
+import Header from '~/components/Header/Header'
+import { Button } from '~/components/Button/Button'
+import { useUpdateAvailableTime } from '~/api/mentor/mentor'
+import { useAppointmentStore } from '~/container/Appointment/store/AppointmentStore'
+import { Props } from './types'
+import { useToast } from '~/hooks/use-toast'
+
+const DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+const TIMECODES = [...Array(48)]
+
+const AppointmentSection = ({ mentorAvailableTimes }: Props) => {
+    const {
+        selectedItems,
+        select,
+        reset,
+        buildAppointmentList,
+        initSelectedItems,
+    } = useAppointmentStore()
+    const { mutate: saveAppointment } = useUpdateAvailableTime()
+    const { toast } = useToast()
+
+    const onReset = () => {
+        const result = window.confirm(
+            'Are you sure to reset appointment time？'
+        )
+        if (result) {
+            reset()
+        }
+    }
+    const save = () => {
+        const appointment = buildAppointmentList()
+        saveAppointment(
+            { availableTimeList: appointment },
+            {
+                onSuccess(res) {
+                    toast({
+                        title: 'Save successful',
+                        variant: 'hint',
+                    })
+                },
+            }
+        )
+    }
+
+    useEffect(() => {
+        initSelectedItems(mentorAvailableTimes || [])
+    }, [mentorAvailableTimes])
+
+    return (
+        <>
+            <Header />
+            <div className="flex flex-col items-center gap-10 py-4">
+                <div className="flex flex-col items-center gap-5 pt-4">
+                    <p className="mb-5 text-3xl">
+                        Select your availability Times
+                    </p>
+                    <div className="flex gap-4">
+                        <Button className="min-w-40 max-w-80" onClick={onReset}>
+                            Reset All
+                        </Button>
+                        <Button className="min-w-40 max-w-80" onClick={save}>
+                            Save
+                        </Button>
+                    </div>
+                </div>
+                <table className="w-full max-w-5xl">
+                    <thead>
+                        <tr className="flex">
+                            <TimeZoneGrid />
+                            {DAYS.map((week, index) => (
+                                <WeekGrid key={index} week={week} />
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {TIMECODES.map((_, rowIndex) => (
+                            <tr key={rowIndex} className="flex">
+                                <HourGrid hour={rowIndex} />
+                                {DAYS.map((_, colIndex) => (
+                                    <ButtonGrid
+                                        key={colIndex}
+                                        variant={
+                                            selectedItems[
+                                                `${colIndex + 1}-${rowIndex}`
+                                            ]
+                                                ? 'primary'
+                                                : 'secondary'
+                                        }
+                                        onClick={() => {
+                                            select(colIndex + 1, rowIndex)
+                                        }}
+                                    />
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
+
+export default AppointmentSection
